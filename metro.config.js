172bf63868ee381cc6fa2563301b86e1589993c7
John Venditti <jdv005@bucknell.edu>
2026-03-22 11:54:36 -0700

    GARDEN-003 add Supabase + PowerSync integration foundation

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// PowerSync requires cjs modules to be resolvable
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'cjs'];
+
 module.exports = config;
